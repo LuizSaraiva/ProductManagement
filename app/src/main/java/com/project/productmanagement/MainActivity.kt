4a952e65a282e97356c34b5439dc88936942e5a5
luiz henrique <l.henrique.saraiva@gmail.com>
2021-05-04 11:23:03 -0300

    created menu navigation

--- app/src/main/java/com/project/productmanagement/MainActivity.kt
+++ app/src/main/java/com/project/productmanagement/MainActivity.kt
@@ -2,10 +2,50 @@ package com.project.productmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var nav: NavigationView
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawer: DrawerLayout
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_main)
+
+        nav = findViewById(R.id.nav)
+        toolbar = findViewById(R.id.toolbar)
+        drawer = findViewById(R.id.drawer_main)
+
+
+        nav.setNavigationItemSelectedListener(this)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.open,
+            R.string.close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_entry -> {
+            }
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
